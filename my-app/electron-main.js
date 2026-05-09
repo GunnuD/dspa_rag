5ddb1d9e1c0de46b46f49df0b6bc0ab6306a355b
Digambar Singh <digambar.singh@vml.com>
2026-05-09 16:14:53 +0530

    Add Windows desktop support using Electron and GitHub Actions configuration

---
+++ my-app/electron-main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    title: "VML RAG Chatbot",
+  });
+
+  // Points to the Expo web build or local dev server
+  const startUrl = isDev 
+    ? 'http://localhost:8081' 
+    : `file://${path.join(__dirname, '../dist/index.html')}`;
+
+  win.loadURL(startUrl);
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
